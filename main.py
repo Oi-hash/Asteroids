f2bfce82e9dcb47ad740825d20e5bf20ae151132
Oi-hash <wzimocha@outlook.com>
2024-11-04 23:53:13 +0000

    G: modify main.py, .gitignore

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
+	pygame.init()
 	print(f'Starting asteroids!')
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print(f'Screen width: {SCREEN_WIDTH}')
 	print(f'Screen height: {SCREEN_HEIGHT}')
+	# Background color
+	color = (0, 0, 0)
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill(color, rect=None)
+		pygame.display.flip()
 
 if __name__ == "__main__":
 	main()
