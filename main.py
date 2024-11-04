69d12645014da50ad5a7d0e7c9c5aa62eccd6a3c
Oi-hash <wzimocha@outlook.com>
2024-11-04 23:31:24 +0000

    E: modify main.py / import constants.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # Import pygame
 import pygame
 
+# Import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+
 def main():
-	print("Starting asteroids!")
+	print(f'Starting asteroids!')
+	print(f'Screen width: {SCREEN_WIDTH}')
+	print(f'Screen height: {SCREEN_HEIGHT}')
 
 if __name__ == "__main__":
 	main()
