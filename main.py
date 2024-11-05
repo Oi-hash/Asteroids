f633382642bd3dd0f18affff163ffdd871134aea
Oi-hash <wzimocha@outlook.com>
2024-11-05 02:15:05 +0000

    O: update main.py / creating groups for updatable and drawable objects

--- main.py
+++ main.py
@@ -25,6 +25,14 @@ def main():
 	player_spawn_x = SCREEN_WIDTH / 2
 	player_spawn_y = SCREEN_HEIGHT / 2
 
+	# Groups
+	group_updatable = pygame.sprite.Group()
+	group_drawable = pygame.sprite.Group()
+
+	# Adding player instance to groups
+	Player.containers = (group_updatable, group_drawable)
+
+	# Player instance 
 	player = Player(player_spawn_x, player_spawn_y)
 	
 	# Game loop
@@ -33,16 +41,19 @@ def main():
 			# If window has been closed, shutdown pygame
 			if event.type == pygame.QUIT:
 				return
+		# Updating updatable elements
+		for element in group_updatable:
+			element.update(dt)
 		# Screen fill
 		screen.fill(color, rect=None)
-		# Drawing objects
-		player.draw(screen)
+		# Drawing drawable objects
+		for element in group_drawable:
+			element.draw(screen)
+		# Calc tick / limiting to framerate
+		dt = clock.tick(framerate) / 1000
 		# Screen update
 		pygame.display.flip()
-		# Calc tick
-		dt = clock.tick(framerate) / 1000
-		# Updating player position
-		player.update(dt)
+		
 
 if __name__ == "__main__":
 	main()
